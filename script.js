a7829c8c296d2ea393ef642c9d52097b9aea8b4f
Andrei <andrewten22@yandex.ru>
2020-01-02 15:08:36 +0200

    add css and normal html

--- script.js
+++ script.js
@@ -1,5 +1,33 @@
 'use strict';
 
+let id = document.getElementById('start');
+let budgetValue = document.getElementsByClassName('budget-value')[0];
+let daybudgetValue = document.getElementsByClassName('daybudget-value')[0];
+let levelValue = document.getElementsByClassName('level-value')[0];
+let expensesValue = document.getElementsByClassName('expenses-value')[0];
+let optionalexpensesValue = document.getElementsByClassName('optionalexpenses-value')[0];
+let incomeValue = document.getElementsByClassName('income-value')[0];
+let monthsavingsValue = document.getElementsByClassName('monthsavings-value')[0];
+let yearsavingsValue = document.getElementsByClassName('yearsavings-value')[0];
+
+
+let expensesItem = document.getElementsByClassName('expenses-item'),
+    expensesBtn = document.getElementsByTagName('button')[0],
+    
+    optionalExpensesItem=document.querySelectorAll('.optionalexpenses-item'),
+    optionalexpensesBtn = document.getElementsByTagName('button')[1],
+    
+    countBtn = document.getElementsByTagName('button')[2],
+
+    chooseIncome = document.querySelector('.choose-income'),
+    checkSavings = document.querySelector('#savings'),
+    sumValue = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+
+    chooseYear = document.querySelector('.year-value'),
+    chooseMonth = document.querySelector('.month-value'),
+    chooseDay = document.querySelector('.day-value');
+
 let money,time;
 
 function start(){
@@ -79,7 +107,7 @@ start();
         appData.income.push(prompt('Может чтото еще?'));
         appData.income.sort();
 
-        appData.income.forEach(function(item, i,mass){
+        appData.income.forEach(function(item, i, mass){
         console.log(++i +"." + " Способы доп заработка: "+ item);
         });
     }     
